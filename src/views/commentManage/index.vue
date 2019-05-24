0b851196c4e158c01673ec66ed818db7021d30f7
yuanhui <1207915357@qq.com>
2019-05-24 18:11:05 +0800

    添加评论管理

--- src/views/commentManage/index.vue
+++ src/views/commentManage/index.vue
@@ -1,6 +1,65 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">评论管理</div>
+  <div class="container">
+    <el-table
+    :data="commentList"
+    style="width: 100%"
+    row-key="created_time"
+    border
+    >
+    <el-table-column align="center" label="#" width="70">
+        <template slot-scope="scope">
+          {{ scope.$index+1 }}
+        </template>
+      </el-table-column>
+    <el-table-column
+      prop="created_time"
+      :formatter="formatTableTime"
+      label="时间"
+      width="180">
+    </el-table-column>
+     <el-table-column
+      prop="user.userName"
+      label="用户"
+      width="180">
+    </el-table-column>
+     <el-table-column
+      label="评论对象"
+      width="180">
+      <template slot-scope="scope">
+        <span v-if="scope.row.articleId">{{scope.row.articleTitle}}</span>
+        <span v-else>@{{scope.row.toUser.userName}}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="content"
+      label="评论内容"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="checkStatus"
+      label="审核状态">
+      <template slot-scope="scope">
+          <el-select placeholder="默认通过" v-model="checkStatus[scope.$index]">
+            <el-option label="折叠" value="折叠"></el-option>
+            <el-option label="删除" value="删除"></el-option>
+          </el-select>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="action"
+      label="操作">
+      <template slot-scope="scope">
+         <el-button plain size='mini' type="primary" @click="handleReply(scope.$index, scope.row)">回复</el-button>
+         <el-button plain size='mini' type='danger'  @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <el-pagination
+    style="text-align:center;margin-top:10px;"
+    background
+    layout="prev, pager, next"
+    :total="1000">
+  </el-pagination>
   </div>
 </template>
 
@@ -13,7 +72,44 @@ export default {
     ...mapGetters([
       'name'
     ])
-  }
+  },
+  data() {
+      return {
+        actionStatus:[],
+        checkStatus:[],
+        commentList:[],
+      }
+    },
+    methods: {
+      //格式化时间
+      formatTableTime(row){
+       return this.parseTime(row.created_time)
+      },
+      //获取所有评论列表
+      getCommentList(){
+        this.$api.article.getCommentList(
+          {start:0,row:10}
+        ).then(data=>{
+          if(data.code==1){
+            this.commentList = data.data
+            for(let ele of this.commentList){
+              ele.children = ele.sub_comment
+            }
+          }
+        })
+      },
+      //回复
+      handleReply(index, row) {
+        console.log(index, row);
+      },
+      //删除
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    },
+    mounted() {
+      this.getCommentList()
+    },
 }
 </script>
 
@@ -26,5 +122,14 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+}
+.container /deep/ .el-table__indent{
+  padding-left: 0px!important;
+}
+.container /deep/ .el-table__row--level-1{
+  background:#dce0e4;
+}
+.container /deep/ .hover-row>td{
+  background:none!important;
 }
 </style>
